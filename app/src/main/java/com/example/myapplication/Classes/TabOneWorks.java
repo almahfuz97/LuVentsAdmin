918405d1d82c0ba6aed0e9e2e229cdcd09748d2c
Abdullah Al Mahfuz <almahfuz0327@gmail.com>
2019-12-13 05:03:13 +0600

    New Created Events details storing in Database is finished.

--- app/src/main/java/com/example/myapplication/Classes/TabOneWorks.java
+++ app/src/main/java/com/example/myapplication/Classes/TabOneWorks.java
@@ -1,38 +1,79 @@
 package com.example.myapplication.Classes;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.myapplication.Models.CreateModel;
 import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 import com.rengwuxian.materialedittext.MaterialEditText;
+import com.rengwuxian.materialedittext.validation.METLengthChecker;
 
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Objects;
 
 public class TabOneWorks {
+
 
     Calendar calendar=Calendar.getInstance();
     final int YEAR=calendar.get(Calendar.YEAR);
     final int MONTH=calendar.get(Calendar.MONTH);
     int DATE=calendar.get(Calendar.DATE);
 
-    int hour=calendar.get(Calendar.HOUR);
-    int min=calendar.get(Calendar.MINUTE);
+    int HOUR=calendar.get(Calendar.HOUR);
+    int MIN=calendar.get(Calendar.MINUTE);
+
+    private ProgressDialog progressDialog;
+
+    private StorageTask storageTask;
+
+    StorageReference storageReference= FirebaseStorage.getInstance().getReference("images");
+
+    private String imageUri;
 
     View view;
     Button choosePhoto;
     EditText eventName,address,description;
+    String mEventName,mAddress,mDescription;
+    private int mDate=DATE,mYear=YEAR,mMonth=MONTH,mEndDate2= DATE,mEndYear2=YEAR,mEndMonth2=MONTH,mMin=MIN,mHour=HOUR,mEndMin=MIN,mEndHour=HOUR;
+
     private TextView date,dateEnd,timeEnd,time;
 
     private Context context;
     private Activity mActivity;
+
 
     public TabOneWorks(Context c, Activity activity) {
 
@@ -43,28 +84,88 @@ public class TabOneWorks {
     public void datePicker()
     {
         date=mActivity.findViewById(R.id.dateid);
+        dateEnd=mActivity.findViewById(R.id.dateEndid);
+        time=mActivity.findViewById(R.id.timeid);
+        timeEnd=mActivity.findViewById(R.id.timeEndid);
+
+        date.setError(null);
+        dateEnd.setError(null);
+        timeEnd.setError(null);
+        time.setError(null);
 
         DatePickerDialog datePickerDialog=new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int Year, int Month, int Date) {
-                String dates=Date+"/"+(Month+1)+"/"+Year;
-                date.setText(dates);
+
+                mYear=Year;
+                mMonth=Month;
+                mDate=Date;
+                if(!((mYear>YEAR )|| (mYear==YEAR && mMonth>MONTH) || (mYear==YEAR && MONTH==mMonth && mDate>=DATE)))
+                {
+                    date.setError("Select an aprropriate date!");
+                    date.requestFocus();
+                    return;
+                }
+                else
+                {
+                    date.setError(null);
+                    dateEnd.setError(null);
+                    timeEnd.setError(null);
+                    time.setError(null);
+                    String dates=Date+"/"+(Month+1)+"/"+Year;
+                    date.setText(dates);
+                }
+
+
+
+
+
+
 
 
             }
         },YEAR,MONTH,DATE);
+
         datePickerDialog.show();
 
     }
    public void dateEndPicker()
     {
+        date=mActivity.findViewById(R.id.dateid);
         dateEnd=mActivity.findViewById(R.id.dateEndid);
+        time=mActivity.findViewById(R.id.timeid);
+        timeEnd=mActivity.findViewById(R.id.timeEndid);
+
+        date.setError(null);
+        dateEnd.setError(null);
+        timeEnd.setError(null);
+        time.setError(null);
 
         DatePickerDialog datePickerDialog=new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int Year, int Month, int Date) {
-                String dates=Date+"/"+(Month+1)+"/"+Year;
-                dateEnd.setText(dates);
+
+                mEndYear2=Year;
+                mEndMonth2=Month;
+                mEndDate2=Date;
+
+                if(!((mEndYear2>mYear )|| (mYear==mEndYear2 && mMonth<mEndMonth2) || (mYear==mEndYear2 && mEndMonth2==mMonth && mDate<=mEndDate2)))
+                {
+                    dateEnd.setError("Select an aprropriate date!");
+                    dateEnd.requestFocus();
+                    return;
+                }
+                else
+                {
+                    dateEnd.setError(null);
+                    time.setError(null);
+                    date.setError(null);
+                    timeEnd.setError(null);
+                    String dates=Date+"/"+(Month+1)+"/"+Year;
+                    dateEnd.setText(dates);
+                }
+
+
 
 
             }
@@ -75,39 +176,242 @@ public class TabOneWorks {
 
     public void timePicker()
     {
-
-
+        date=mActivity.findViewById(R.id.dateid);
+        dateEnd=mActivity.findViewById(R.id.dateEndid);
         time=mActivity.findViewById(R.id.timeid);
+        timeEnd=mActivity.findViewById(R.id.timeEndid);
+
+        date.setError(null);
+        dateEnd.setError(null);
+        timeEnd.setError(null);
+        time.setError(null);
+
+
+
         TimePickerDialog timePickerDialog=new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker timePicker, int hour, int min) {
-                String times=String.format("%02d:%02d",hour,min);
-                time.setText(times);
+                mMin=min;
+                mHour=hour;
+
+                if(!((mYear>YEAR) || (mYear==YEAR && mMonth>MONTH) || (mYear==YEAR && mMonth==MONTH && mDate>DATE) || (mYear==YEAR && mMonth==MONTH && mDate==DATE && (mHour>HOUR || (mHour==HOUR && mMin>MIN)))))
+                {
+
+                    time.setError("Select an appropriate time!");
+                    time.requestFocus();
+                    return;
+                }
+                else
+                {
+                    time.setError(null);
+                    date.setError(null);
+                    dateEnd.setError(null);
+                    timeEnd.setError(null);
+                    String times=String.format("%02d:%02d",hour,min);
+                    time.setText(times);
+                }
+
 
             }
-        },hour,min,true);
+        },HOUR,MIN,true);
 
         timePickerDialog.show();
 
     }
     public void timeEndPicker()
     {
-
+        date=mActivity.findViewById(R.id.dateid);
+        dateEnd=mActivity.findViewById(R.id.dateEndid);
+        time=mActivity.findViewById(R.id.timeid);
         timeEnd=mActivity.findViewById(R.id.timeEndid);
+
+        date.setError(null);
+        dateEnd.setError(null);
+        timeEnd.setError(null);
+        time.setError(null);
         TimePickerDialog timePickerDialog=new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
             @Override
-            public void onTimeSet(TimePicker timePicker, int hour, int min) {
+            public void onTimeSet(TimePicker timePicker, int hour, int min)
 
-                String times=String.format("%02d:%02d",hour,min);
-                timeEnd.setText(times);
+            {
+                mEndHour=hour;
+                mEndMin=min;
+
+                if((mMonth==mEndMonth2 && mDate==mEndDate2 && mYear==mEndYear2 && mHour>=mEndHour ) )
+                {
+                    timeEnd.setError("Select an appropriate time");
+                    timeEnd.requestFocus();
+                    return;
+                }
+                else
+                {
+                    timeEnd.setError(null);
+                    date.setError(null);
+                    time.setError(null);
+                    dateEnd.setError(null);
+                    String times=String.format("%02d:%02d",hour,min);
+                    timeEnd.setText(times);
+                }
+
+
 
             }
-        },hour,min,true);
+        },HOUR,MIN,true);
         timePickerDialog.show();
 
     }
-    public void createEvent()
+
+
+
+
+
+
+    public void createEvent(Uri mUri,String extension) {
+        eventName = mActivity.findViewById(R.id.eventnameEdittextId);
+        address = mActivity.findViewById(R.id.addressid);
+        description = mActivity.findViewById(R.id.descriptionId);
+        date = mActivity.findViewById(R.id.dateid);
+        dateEnd = mActivity.findViewById(R.id.dateEndid);
+        time = mActivity.findViewById(R.id.timeid);
+        timeEnd = mActivity.findViewById(R.id.timeEndid);
+
+        final String mEventname = eventName.getText().toString().trim();
+        final String mAddress = address.getText().toString().trim();
+        final String mDescription = description.getText().toString();
+
+
+        //
+        //time date error checking
+        if (mEventname.isEmpty()) {
+            eventName.setError("Name can't be empty!");
+            eventName.requestFocus();
+            return;
+        }
+        if (mAddress.isEmpty()) {
+            address.setError("Address can't be empty!");
+            address.requestFocus();
+            return;
+        }
+
+
+        if (!((mYear > YEAR) || (mYear == YEAR && mMonth > MONTH) || (mYear == YEAR && MONTH == mMonth && mDate >= DATE))) {
+            date.setError("Select an aprropriate date!");
+            date.requestFocus();
+            return;
+        } else {
+            date.setError(null);
+        }
+        if (!((mEndYear2 > mYear) || (mYear == mEndYear2 && mMonth < mEndMonth2) || (mYear == mEndYear2 && mEndMonth2 == mMonth && mDate <= mEndDate2))) {
+            dateEnd.setError("Select an aprropriate date!");
+            dateEnd.requestFocus();
+            return;
+        } else {
+            dateEnd.setError(null);
+
+        }
+        if (!((mYear > YEAR) || (mYear == YEAR && mMonth > MONTH) || (mYear == YEAR && mMonth == MONTH && mDate > DATE) || (mYear == YEAR && mMonth == MONTH && mDate == DATE && (mHour > HOUR || (mHour == HOUR && mMin > MIN))))) {
+
+            time.setError("Select an appropriate time!");
+            time.requestFocus();
+            return;
+        } else {
+            time.setError(null);
+
+        }
+        if ((mMonth == mEndMonth2 && mDate == mEndDate2 && mYear == mEndYear2 && mHour >= mEndHour)) {
+            timeEnd.setError("Select an appropriate time");
+            timeEnd.requestFocus();
+            return;
+        } else {
+            timeEnd.setError(null);
+
+        }
+
+
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Creating Event...");
+        progressDialog.show();
+
+
+        //Adding to firestore
+
+        if (mUri == null) {
+            imageUri = "No Image";
+
+            storeInDatabase(mEventname, mAddress, mDescription);
+
+
+        } else {
+
+
+
+
+            final StorageReference fileRef = storageReference.child(System.currentTimeMillis()
+                    + "." + extension);
+
+            storageTask = fileRef.putFile(mUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    imageUri = uri.toString();
+
+                                    storeInDatabase(mEventname, mAddress, mDescription);
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+        }
+    }
+
+    private void storeInDatabase(String mName,String mAdd,String mDesc)
     {
+        mEventName=mName;
+        mAddress=mAdd;
+        mDescription=mDesc;
+        CreateModel createModel=new CreateModel(mEventName,mAddress,mDescription,imageUri,"gfgj",mYear,mMonth+1,mDate,mEndYear2,mEndMonth2+1,mEndDate2,mHour,mMin,mEndHour,mEndMin,System.currentTimeMillis());
+        FirebaseFirestore.getInstance().collection("Events").add(createModel).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                progressDialog.cancel();
+
+                CreateModel createModel2=new CreateModel(mEventName,mAddress,mDescription,imageUri,task.getResult().getId(),mYear,mMonth+1,mDate,mEndYear2,mEndMonth2+1,mEndDate2,mHour,mMin,mEndHour,mEndMin,System.currentTimeMillis());
+
+                FirebaseFirestore.getInstance().collection("Events").document(task.getResult().getId()).set(createModel2).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(context, "Upload Succesfull", Toast.LENGTH_LONG).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(context, "Not Succesfull", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.cancel();
+                Toast.makeText(context, "Failed!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
 
     }
 
--- app/src/main/java/com/example/myapplication/Fragments/Tab1.java
+++ app/src/main/java/com/example/myapplication/Fragments/Tab1.java
@@ -1,27 +1,43 @@
 package com.example.myapplication.Fragments;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.example.myapplication.Classes.TabOneWorks;
 import com.example.myapplication.R;
 import com.rengwuxian.materialedittext.MaterialEditText;
+import com.squareup.picasso.Picasso;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -32,10 +48,21 @@ import java.util.Calendar;
  * create an instance of this fragment.
  */
 public class Tab1 extends Fragment implements View.OnClickListener {
+
+    public Tab1() {
+        // Required empty public constructor
+    }
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private int  STORAGE_PERMISSION_CODE = 1;
+    private Uri mUri;
+    private  String ImageUri;
+    int x=0;
+
 
     Calendar calendar=Calendar.getInstance();
     final int YEAR=calendar.get(Calendar.YEAR);
@@ -48,6 +75,7 @@ public class Tab1 extends Fragment implements View.OnClickListener {
     View view;
     Button choosePhoto,createEvent;
     EditText eventName,address,description;
+    ImageView imageView;
     TextView date,time,dateEnd,timeEnd;
 
     TabOneWorks tabOneWorks;
@@ -58,9 +86,7 @@ public class Tab1 extends Fragment implements View.OnClickListener {
 
     private OnFragmentInteractionListener mListener;
 
-    public Tab1() {
-        // Required empty public constructor
-    }
+
 
     /**
      * Use this factory method to create a new instance of
@@ -95,21 +121,24 @@ public class Tab1 extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         view= inflater.inflate(R.layout.fragment_tab1, container, false);
 
-        eventName=view.findViewById(R.id.nameEdittextId);
-        address=view.findViewById(R.id.addressid);
-        description=view.findViewById(R.id.descriptionId);
+//        eventName=view.findViewById(R.id.nameEdittextId);
+//        address=view.findViewById(R.id.addressid);
+//        description=view.findViewById(R.id.descriptionId);
 
         date=view.findViewById(R.id.dateid);
         time=view.findViewById(R.id.timeid);
         dateEnd=view.findViewById(R.id.dateEndid);
         timeEnd=view.findViewById(R.id.timeEndid);
         createEvent=view.findViewById(R.id.createEventbtnid);
+        imageView=view.findViewById(R.id.imageviewid);
+        choosePhoto=view.findViewById(R.id.choosephotoid);
 
         date.setOnClickListener(this);
         time.setOnClickListener(this);
         dateEnd.setOnClickListener(this);
         timeEnd.setOnClickListener(this);
         createEvent.setOnClickListener(this);
+        choosePhoto.setOnClickListener(this);
 
         String dates=DATE+"/"+(MONTH+1)+"/"+YEAR;
         date.setText(dates);
@@ -169,11 +198,106 @@ public class Tab1 extends Fragment implements View.OnClickListener {
                 break;
             case R.id.timeEndid:
                 tabOneWorks.timeEndPicker();
+                break;
             case R.id.createEventbtnid:
-                tabOneWorks.createEvent();
+                if(x==1)
+                {
+                    tabOneWorks.createEvent(mUri,getFileExtension(mUri));
+                }
+                else tabOneWorks.createEvent(mUri,"");
+
+                break;
+            case R.id.choosephotoid:
+                choosePhoto();
+                break;
         }
 
     }
+
+    //setting Image
+
+    public void choosePhoto()
+    {
+        if (ContextCompat.checkSelfPermission(Objects.requireNonNull(getActivity()),
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+
+            Intent intent = new Intent().setType("image/*").setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(intent, PICK_IMAGE_REQUEST);
+
+
+        } else {
+            requestStoragePermission();
+
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==PICK_IMAGE_REQUEST && resultCode== Activity.RESULT_OK && data!=null && data.getData()!=null)
+        {
+            mUri=data.getData();
+            Picasso.get().load(mUri).fit().into(imageView);
+            x=1;
+
+            //Toast.makeText(getActivity(), "Successfull", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void requestStoragePermission()
+    {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(Objects.requireNonNull(getActivity()),
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            new AlertDialog.Builder(getActivity())
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed because of this and that")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(getActivity(),
+                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+
+
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getActivity(), "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getActivity(), "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(resolver.getType(uri));
+    }
+
+
+//end of image setting process
 
 
     /**
